200bed84f4ba3994b1b6612f8665030139e9226e
asnashameel <2020.cse.asna@ekc.edu.in>
2025-05-31 05:25:56 +0000

    chaged

--- tests_main.py
+++ tests_main.py
@@ -1,9 +1,11 @@
 from main import app
 
 def test_hello():
-    # Create a test client using the Flask application configured for testing
-    client = app.test_client()
-    response = client.get('/')
+    app.testing = True  # Enable testing mode
     
-    assert response.status_code == 200
-    assert response.data.decode('utf-8') == 'Hello world with Flask'
+    with app.test_client() as client:
+        response = client.get('/')
+        
+        assert response.status_code == 200
+        assert response.data.decode('utf-8') == 'Hello world with Flask'
+
